be8d74675a99a69abe346deb4a23482a1a5f9b99
Yutaro Iiyama <yiiyama@mit.edu>
2019-04-05 09:08:10 -0400

    configuration bugfix

--- confs/DarkPhotonZHM200Summer16/conf.sh
+++ confs/DarkPhotonZHM200Summer16/conf.sh
@@ -1,4 +1,4 @@
 PANDA_VERSION=005
 MINIAOD_CAMPAIGN=RunIISummer16MiniAODv2
 GRIDPACKS="gsiftp://eoscmsftp.cern.ch:2811//eos/cms/store/user/yiiyama/gridpacks/HZJ_slc6_amd64_gcc700_CMSSW_9_3_0_HZJ_HanythingJ_NNPDF30_13TeV_M200_Vleptonic_HZJ.tgz"
-NCPU=1
+NCPU=8
--- confs/DarkPhotonZHM300Autumn18/conf.sh
+++ confs/DarkPhotonZHM300Autumn18/conf.sh
@@ -1,4 +1,4 @@
 PANDA_VERSION=013
 MINIAOD_CAMPAIGN=RunIIAutumn18MiniAOD
-NCPU=1
+NCPU=8
 GRIDPACKS="gsiftp://eoscmsftp.cern.ch:2811//eos/cms/store/user/yiiyama/gridpacks/HZJ_slc6_amd64_gcc700_CMSSW_9_3_0_HZJ_HanythingJ_NNPDF31_13TeV_M300_ZtoLL.tgz"
